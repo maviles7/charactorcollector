b0349dcf7876d95d01c389529854bbc6fc772c42
maviles <maviles221@gmail.com>
2024-09-26 11:04:19 -0500

    part 1

--- charactorcollector/settings.py
+++ charactorcollector/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'charactorcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'charactercollector',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
 
